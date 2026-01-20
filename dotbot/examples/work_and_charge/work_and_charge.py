f9442ecc10333c0e5ba69c1aa1bd90b956595b30
Genki Miyauchi <g.miyauchi@sheffield.ac.uk>
2026-01-20 14:32:41 +0000

    cleanup code for the simulator example

--- dotbot/examples/work_and_charge/work_and_charge.py
+++ dotbot/examples/work_and_charge/work_and_charge.py
@@ -35,9 +35,6 @@ QUEUE_SPACING = (
     0.1  # Spacing between consecutive bots in the charging queue (along X axis)
 )
 
-# (PARK_X, PARK_Y) = (0.8, 0.1)  # World-frame (X, Y) position of the parking area origin
-# PARK_SPACING = 0.1  # Spacing between parked bots (along Y axis)
-
 
 dotbot_controllers = dict()
 
@@ -172,157 +169,6 @@ class Controller:
         return False
 
 
-# async def queue_robots(
-#     client: RestClient,
-#     dotbots: List[DotBotModel],
-#     params: OrcaParams,
-# ) -> None:
-#     sorted_bots = order_bots(dotbots, QUEUE_HEAD_X, QUEUE_HEAD_Y)
-#     goals = assign_queue_goals(sorted_bots, QUEUE_HEAD_X, QUEUE_HEAD_Y, QUEUE_SPACING)
-#     await send_to_goal(client, goals, params)
-
-
-# async def charge_robots(
-#     client: RestClient,
-#     params: OrcaParams,
-# ) -> None:
-#     dotbots = await client.fetch_active_dotbots()
-#     remaining = order_bots(dotbots, QUEUE_HEAD_X, QUEUE_HEAD_Y)
-#     total_count = len(dotbots)
-#     # The head of the remaining should park
-#     # Except on the first loop, where it should just queue.
-#     park_dotbot: DotBotModel | None = None
-#     parked_count = total_count - len(remaining)
-
-#     while remaining or park_dotbot is not None:
-#         dotbots = await client.fetch_active_dotbots()
-
-#         dotbots = [b for b in dotbots if b.address in {r.address for r in remaining}]
-#         remaining = order_bots(dotbots, QUEUE_HEAD_X, QUEUE_HEAD_Y)
-
-#         # Assign charging + shift goals
-#         goals = assign_charge_goals(
-#             remaining, QUEUE_HEAD_X, QUEUE_HEAD_Y, QUEUE_SPACING
-#         )
-
-#         if park_dotbot is not None:
-#             goals[park_dotbot.address] = {
-#                 "x": PARK_X,
-#                 "y": PARK_Y + parked_count * PARK_SPACING,
-#             }
-#         await send_to_goal(client, goals, params)
-
-#         if len(remaining) == 0:
-#             break
-
-#         head = remaining[0]
-
-#         # Cosmetic: wait for charging...
-#         colors = [
-#             (255, 255, 0),  # yellow
-#             (0, 255, 0),  # green
-#         ]
-#         await asyncio.sleep(20 * DT)
-
-#         for r, g, b in colors:
-#             await client.send_rgb_led_command(
-#                 address=head.address,
-#                 command=DotBotRgbLedCommandModel(red=r, green=g, blue=b),
-#             )
-
-#             await asyncio.sleep(20 * DT)
-
-#         # Reverse slightly to disengage the robot from the charging station
-#         await disengage_from_charger(client, head)
-
-#         parked_count = total_count - len(remaining)
-
-#         # send it to park
-#         park_dotbot = remaining[0]
-#         # Remove head from queue
-#         remaining = remaining[1:]
-
-
-# async def disengage_from_charger(client: RestClient, dotbot: DotBotModel):
-#     for _ in range(25):
-#         await client.send_move_raw_command(
-#             address=dotbot.address,
-#             application=dotbot.application,
-#             command=DotBotMoveRawCommandModel(
-#                 left_x=0, left_y=-100, right_x=0, right_y=-100
-#             ),
-#         )
-#         await asyncio.sleep(DT)
-
-
-# async def send_to_goal(
-#     client: RestClient,
-#     goals: Dict[str, dict],
-#     params: OrcaParams,
-# ) -> None:
-#     while True:
-#         dotbots = await client.fetch_active_dotbots()
-#         agents: List[Agent] = []
-
-#         for bot in dotbots:
-#             agents.append(
-#                 Agent(
-#                     id=bot.address,
-#                     position=Vec2(x=bot.lh2_position.x, y=bot.lh2_position.y),
-#                     velocity=Vec2(x=0, y=0),
-#                     radius=BOT_RADIUS,
-#                     direction=bot.direction,
-#                     max_speed=MAX_SPEED,
-#                     preferred_velocity=preferred_vel(
-#                         dotbot=bot, goal=goals.get(bot.address)
-#                     ),
-#                 )
-#             )
-
-#         queue_ready = all(
-#             a.preferred_velocity.x == 0 and a.preferred_velocity.y == 0 for a in agents
-#         )
-#         if queue_ready:
-#             break
-#         for agent in agents:
-#             neighbors = [neighbor for neighbor in agents if neighbor.id != agent.id]
-
-#             orca_vel = await compute_orca_velocity(
-#                 agent, neighbors=neighbors, params=params
-#             )
-#             STEP_SCALE = 0.1
-#             step = Vec2(x=orca_vel.x * STEP_SCALE, y=orca_vel.y * STEP_SCALE)
-
-#             # ---- CLAMP STEP TO GOAL DISTANCE ----
-#             goal = goals.get(agent.id)
-#             if goal is not None:
-#                 dx = goal["x"] - agent.position.x
-#                 dy = goal["y"] - agent.position.y
-#                 dist_to_goal = math.hypot(dx, dy)
-
-#                 step_len = math.hypot(step.x, step.y)
-#                 if step_len > dist_to_goal and step_len > 0:
-#                     scale = dist_to_goal / step_len
-#                     step = Vec2(x=step.x * scale, y=step.y * scale)
-#             # ------------------------------------
-
-#             waypoints = DotBotWaypoints(
-#                 threshold=THRESHOLD,
-#                 waypoints=[
-#                     DotBotLH2Position(
-#                         x=agent.position.x + step.x, y=agent.position.y + step.y, z=0
-#                     )
-#                 ],
-#             )
-#             await client.send_waypoint_command(
-#                 address=agent.id,
-#                 application=ApplicationType.DotBot,
-#                 command=waypoints,
-#             )
-#         await asyncio.sleep(DT)
-#     return None
-
-
 def order_bots(
     dotbots: List[DotBotModel], base_x: int, base_y: int
 ) -> List[DotBotModel]:
@@ -350,32 +196,6 @@ def assign_goals(
             "y": head_y + i * spacing,
         }
     return goals
-
-
-# def assign_charge_goals(
-#     ordered: List[DotBotModel],
-#     base_x: int,
-#     base_y: int,
-#     spacing: int,
-# ) -> Dict[str, dict]:
-#     if len(ordered) == 0:
-#         return {}
-
-#     goals = {}
-#     # Send the first one to the charger
-#     head = ordered[0]
-#     goals[head.address] = {
-#         "x": 0.2,
-#         "y": 0.2,
-#     }
-
-#     # Remaining bots shift left in the queue
-#     for i, bot in enumerate(ordered[1:]):
-#         goals[bot.address] = {
-#             "x": base_x + i * spacing,
-#             "y": base_y,
-#         }
-#     return goals
 
 
 def preferred_vel(dotbot: DotBotModel, goal: Vec2 | None) -> Vec2:
@@ -444,13 +264,14 @@ async def main() -> None:
 
     dotbots = await client.fetch_active_dotbots()
 
-    # Cosmetic: all bots are red
+    # Initialization
     for dotbot in dotbots:
 
         # Init controller
         controller = Controller(dotbot.address, sct_path)
         dotbot_controllers[dotbot.address] = controller        
 
+        # Cosmetic: all bots are red
         await client.send_rgb_led_command(
             address=dotbot.address,
             command=DotBotRgbLedCommandModel(red=255, green=0, blue=0),
@@ -470,6 +291,7 @@ async def main() -> None:
         waypoint_work = DotBotLH2Position(x=goal['x'], y=goal['y'], z=0)
         controller.set_work_waypoint(waypoint_work)
 
+    # Simulation loop
     while True:
 
         print('\n', end='')
@@ -484,6 +306,7 @@ async def main() -> None:
 
             # print(f'waypoint_current for DotBot {bot.address}: {dotbot_controllers.get(bot.address).waypoint_current}')
 
+            # Get current goals
             if dotbot_controllers.get(bot.address).waypoint_current is not None:
                 goals[bot.address] = {
                     "x": dotbot_controllers[bot.address].waypoint_current.x,
